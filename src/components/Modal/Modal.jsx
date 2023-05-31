b52efbfa4242bf3a3c56b97f003ba6d20a73d9ed
Nikita Tsurikhin <tsurikhinnikita@gmail.com>
2023-06-01 01:37:44 +0600

    hooks2

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,3 @@
-// import React, { Component } from 'react';
-// import s from './Modal.module.css';
-
-// class Modal extends Component {
-//   componentDidMount() {
-//     document.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = event => {
-//     if (event.keyCode === 27) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleOverlayClick = event => {
-//     if (event.target === event.currentTarget) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     const { imageUrl } = this.props;
-
-//     return (
-//       <>
-//         <div className={s.Overlay} onClick={this.handleOverlayClick}>
-//           <div className={s.Modal}>
-//             <img src={imageUrl} alt="" />
-//           </div>
-//         </div>
-//       </>
-//     );
-//   }
-// }
-
-// export default Modal;
-
 import React, { useEffect, useCallback } from 'react';
 import s from './Modal.module.css';
 
@@ -59,11 +18,17 @@ const Modal = ({ imageUrl, onClose }) => {
   };
 
   useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.keyCode === 27) {
+        onClose();
+      }
+    };
+
     document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [handleKeyDown, onClose]);
 
   return (
     <>
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,35 +1,3 @@
-// import React, { Component } from 'react';
-// import s from './Searchbar.module.css';
-
-// export default class Searchbar extends Component {
-//   onChangeQuery = event => {
-//     event.preventDefault();
-//     const newSearchTerm = event.target.elements.search.value;
-//     this.props.onSubmit(newSearchTerm);
-//   };
-
-//   render() {
-//     return (
-//       <header className={s.Searchbar}>
-//         <form className="form" onSubmit={this.onChangeQuery}>
-//           <button type="submit" className="button">
-//             <span className="button-label">Search</span>
-//           </button>
-
-//           <input
-//             className="input"
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//             name="search"
-//           />
-//         </form>
-//       </header>
-//     );
-//   }
-// }
-
 import React, { useState, useCallback } from 'react';
 import s from './Searchbar.module.css';
 
